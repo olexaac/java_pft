34bbd6f48ff52d518a3cb5048281a2e73cc7d0c4
olexaac <arbeidnsk@gmail.com>
2017-02-22 18:55:30 +0600

    Изменение существующей группы (тест без использования рекордера)

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by Админ on 22.02.2017.
+ */
+public class GroupModificationTest extends TestBase {
+  @Test
+  public void GroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
