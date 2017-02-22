f5da42cb6d0fff2348c62a4838fe502417428a26
olexaac <arbeidnsk@gmail.com>
2017-02-22 17:43:20 +0600

    Заменено наследование на делегирование, выделены классы помощники

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Админ on 22.02.2017.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() {
+      wd.findElement(By.linkText("groups")).click();
+  }
+}
