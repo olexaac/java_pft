0c2c9a50b41f0c5b7d257013ae0c005069164ab4
olexaac <arbeidnsk@gmail.com>
2017-04-18 23:55:00 +0600

    Реализована проверка и обеспечение выполнения предусловий

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -10,6 +10,9 @@ public class GroupModificationTest extends TestBase {
   @Test
   public void GroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
